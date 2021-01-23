12c2d24ecf4bffdf7adb3908e1e1bf2996c2fe8a
Cathgao <cath@stu.zsc.edu.cn>
2021-01-24 03:16:02 +0800

    update packages

--- diy.sh
+++ diy.sh
@@ -13,9 +13,12 @@ sed -i 's/config internal themes/config internal themes\n    option Argon  \"\/l
 echo '去除默认bootstrap主题'
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 
+
 echo '删除旧版argon,链接新版'
 rm -rf ./package/lean/luci-theme-argon
-ln -s ../../../luci-theme-argon ./package/lean/
+svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/luci-theme-argonv3 ../diy/luci-theme-argonv3
+svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/luci-app-argon-config ../diy/luci-argon-config
+#ln -s ../../../luci-theme-argon ./package/lean/
 
 echo '修改wifi名称'
 sed -i 's/OpenWrt/G-DOCK/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -28,15 +31,15 @@ echo '下载ServerChan'
 git clone https://github.com/tty228/luci-app-serverchan ../diy/luci-app-serverchan
 
 echo '下载AdGuard Home'
-svn co https://github.com/Lienol/openwrt/trunk/package/diy/luci-app-adguardhome ../diy/luci-app-adguardhome
-svn co https://github.com/Lienol/openwrt/trunk/package/diy/adguardhome ../diy/luci-app-adguardhome
+svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/luci-app-adguardhome
+svn co https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t/AdGuardHome
 
-echo 'JD script'
-git clone https://github.com/Cathgao/luci-app-jd-dailybonus ../diy/luci-app-jd-dailybonus
+#echo 'JD script'
+#git clone https://github.com/Cathgao/luci-app-jd-dailybonus ../diy/luci-app-jd-dailybonus
 
 echo '集成diy目录'
 ln -s ../../diy ./package/openwrt-packages
 
-echo '首页增加CPU频率动态显示'
-cp -f ../diy/mod-index.htm ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
+#echo '首页增加CPU频率动态显示'
+#cp -f ../diy/mod-index.htm ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
 
